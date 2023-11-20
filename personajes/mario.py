69e14f3b134bfcac818c83e1e99a49c428340b34
alejandro <tonto12120@gmail.com>
2023-11-20 15:36:08 +0100

    increased speed of mario

--- personajes/mario.py
+++ personajes/mario.py
@@ -25,35 +25,36 @@ class Mario:
 
     def move(self, ancho: int, dir: int = 0):
         """Este método moverá a Mario
-        
+
         @param ancho: es el ancho del tablero
         @param dir: es un int para almacenar la dirección, -1 si va a la izquierda, 1 si va a la derecha, por defecto 0
         """
-        
+
         # Si la nueva direccion es 0 (actuará como un valor especial)
         # se moverá en la dirección que tenga guardada
         if dir == 0:
             dir = self.__direccion
 
-        # Actualizamos la x de Mario    
-        self.__x += dir
-        
+        # Actualizamos la x de Mario
+        # Multiplicamos por 1.6 para que se mueva más rápido
+        self.__x += dir*1.6
+
         # Si se sale por la derecha, aparece por la izquierda
         if dir == 1 and self.__x >= ancho:
             self.__x = 0
         # Si se sale por la izquierda, aparece por la derecha
         elif dir == -1 and self.__x <= 0:
             self.__x = ancho
-        
+
         # Guardamos la dirección
         self.__direccion = dir
-        
+
         # Cambiamos la animación
         self.__animacion += 1
         # Reiniciamos la animación si se ha pasado
         if self.__animacion >= len(config.MARIO_SPRITE):
             self.__animacion = 0
-        
+
         # Como hemos cambiado la animacion, actualizamos el sprite
         self.__actualizar_sprite()
 
@@ -67,18 +68,19 @@ class Mario:
         # Dibujamos a Mario
         # TODO: Tener en cuenta animaciones
 
-        
         # Si se mueve para la izquierda, se invierte el sprite
         if self.__direccion == -1:
-            pyxel.blt(self.__x, self.__y, self.__sprite[0], self.__sprite[1], self.__sprite[2], -self.__sprite[3], self.__sprite[4], 8)
+            pyxel.blt(self.__x, self.__y, self.__sprite[0], self.__sprite[1],
+                      self.__sprite[2], -self.__sprite[3], self.__sprite[4], 8)
         # Si se mueve para la derecha, se dibuja normal
         else:
-            pyxel.blt(self.__x, self.__y, self.__sprite[0], self.__sprite[1], self.__sprite[2], self.__sprite[3], self.__sprite[4], 8)
-        
+            pyxel.blt(self.__x, self.__y, self.__sprite[0], self.__sprite[1],
+                      self.__sprite[2], self.__sprite[3], self.__sprite[4], 8)
+
     def __actualizar_sprite(self):
         """Este método actualizará el sprite de Mario"""
         self.__sprite = config.MARIO_SPRITE[self.__animacion]
-        
+
     @property
     def vidas(self) -> int:
         """Vidas de Mario"""
